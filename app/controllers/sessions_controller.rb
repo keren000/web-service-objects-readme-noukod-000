8ff6365bc7458def859a0983e6b6d8e0b5960218
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-26 16:21:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
-  def authenticate!(client_id, client_secret, code)   
+class FoursquareService
+    def authenticate!(client_id, client_secret, code)   
         resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
             req.params['client_id'] = client_id
             req.params['client_secret'] = client_secret
@@ -6,9 +7,10 @@
             req.params['redirect_uri'] = "http://localhost:3000/auth"
             req.params['code'] = code
         end
-          body = JSON.parse(resp.body)
-          body["access_token"]
-        end
+        body = JSON.parse(resp.body)
+        body["access_token"]
+    end
+
 
   def friends
       foursquare = FoursquareService.new
