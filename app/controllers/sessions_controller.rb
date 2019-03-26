bef1f0f5daf970b1946556b883e7cabe1bfe44a3
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-26 16:24:08 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,11 @@ class FoursquareService
     end
 
 
-  def friends
-      foursquare = FoursquareService.new
-      @friends = foursquare.friends(session[:token])
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
   end
 end
